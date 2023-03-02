ee69db533a0c5bf4d05453c4c5c30555910ab7cb
wsl <loghoosong@163.com>
2023-03-03 01:47:11 +0800

    commit

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  fileFilter(req, file, cb) {
+    file.originalname = Buffer.from(file.originalname, 'latin1').toString('utf-8');
+    cb(null, true);
+  }
+});
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +20,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 
 const port = process.env.PORT || 3000;
